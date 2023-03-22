20d3e405f32aa6907f4758584767cff57af0a19e
LethalWarrior <albertfiraz@gmail.com>
2023-03-22 17:40:24 +0700

    Fix typos in webpack configs

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const common = reqire('./webpack.common.js');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
